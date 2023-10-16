c4eb50155b87ea41dc794b1b5ade39e3a851281c
branham514 <branham.gatheru@student.moringaschool.com>
2023-10-16 13:09:44 +0300

    fetched data from db.json

---
+++ index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Fetch data from db.json
+  fetch('db.json')
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the fetched data here
+      const carList = data.cars; // Assuming the cars are in an array under the "cars" key
+    })
+  })
